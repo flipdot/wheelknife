b71f1ad29c9696935510833a4e8553426ec16564
Soeren Wegener <wegener92@gmail.com>
2018-11-17 13:47:43 +0100

    Collect data from front and back sensor

--- Wheelknife/src/main.cpp
+++ Wheelknife/src/main.cpp
@@ -8,7 +8,8 @@
 #define BACK_SENSOR_ECHO 34
 
 long duration;
-int distance;
+int front_distance;
+int back_distance;
 
 void setup() {
   // initialize LED digital pin as an output.
@@ -21,19 +22,26 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
+int get_distance(int triggerPin, int echoPin) {
   // Clears the trigPin
-  digitalWrite(FRONT_SENSOR_TRIGGER, LOW);
+  digitalWrite(triggerPin, LOW);
   delayMicroseconds(2);
   // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(FRONT_SENSOR_TRIGGER, HIGH);
+  digitalWrite(triggerPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(FRONT_SENSOR_TRIGGER, LOW);
+  digitalWrite(triggerPin, LOW);
   // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(FRONT_SENSOR_ECHO, HIGH);
+  duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
-  distance = duration*0.034/2;
+  return duration*0.034/2;
+}
+
+void loop() {
+  front_distance = get_distance(FRONT_SENSOR_TRIGGER, FRONT_SENSOR_ECHO);
+  back_distance = get_distance(BACK_SENSOR_TRIGGER, BACK_SENSOR_ECHO);
   // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
+  Serial.print(front_distance);
+  Serial.print(",");
+  Serial.print(back_distance);
+  Serial.println();
 }
