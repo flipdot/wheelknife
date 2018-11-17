20f9edaeb3d9981308a80e57dd2534e60ede9f89
Jörn Schmeißing <joern@schmeissing.org>
2018-11-17 18:21:28 +0100

    Simple Noise Filter and MaFilter

--- Wheelknife/src/average.h
+++ Wheelknife/src/average.h
@@ -4,17 +4,16 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
-using namespace std;
 
 class MovingAverage {
+// Simple Moving Average, which returns the average of the last _length_ buffer elements.
+// _buffer_ is a circular buffer.
 private:
-	double val;
-    vector<double> buffer;
+    std::vector<double> buffer;
     int m_length;
 
 public:
-	MovingAverage(int length) {
-        m_length = length;
+	MovingAverage(int length) : m_length(length) {
         buffer.clear();
 	}
 
@@ -23,9 +22,40 @@ public:
         if( buffer.size() >= m_length )
             buffer.erase(buffer.begin());
         buffer.push_back(x);
-        for(double &b : buffer)
+        for(double &b : buffer) {
             sum += b;
+        }
         return sum/buffer.size();
+    }
+};
+
+class NoiseFilter {
+// Simple Noise Filter, which returns the same value, if it does not differ 
+// more than diff_val from the last value
+private:
+    int m_diff_val;
+    double current_value;
+
+public:
+	NoiseFilter(double diff_val) : m_diff_val(diff_val) {
+        current_value = 0;
+	}
+
+    double update(double x) {
+        if( abs(x-current_value) > m_diff_val )
+            current_value = x;
+        return current_value;
+    }
+};
+
+class MaFilter {
+private:
+    MovingAverage ma;
+    NoiseFilter nf;
+public:
+    MaFilter(int ma_length, double filter_diff) : ma(MovingAverage(ma_length)), nf(NoiseFilter(filter_diff)) {}
+    double update(double x) {
+        return nf.update(ma.update(x));
     }
 };
 
--- Wheelknife/test/test_average.cpp
+++ Wheelknife/test/test_average.cpp
@@ -4,8 +4,32 @@
 
 using namespace std;
 
-int main(void) {
+void test_ma(){
     MovingAverage ma = MovingAverage(3);
+    cout << "Testing Moving Average: ";
     for(int i = 0; i < 10; i++)
-        cout << ma.update(i) << endl;
+        cout << ma.update(i) << " ";
+    cout << endl;
+}
+
+void test_NoiseFilter() {
+    NoiseFilter fi = NoiseFilter(5);
+    cout << "Testing Noise Filter: ";
+    for(int i = 0; i < 10; i++)
+        cout << fi.update(i) << " ";
+    cout << endl;
+}
+
+void test_MaFilter() {
+    MaFilter mf = MaFilter(2, 5);
+    cout << "Testing MaFilter: ";
+    for(int i = 0; i < 10; i++)
+        cout << mf.update(i) << " ";
+    cout << endl;
+}
+
+int main(void) {
+    test_ma();
+    test_NoiseFilter();
+    test_MaFilter();
 }
