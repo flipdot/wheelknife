b7a55118c29e2cdfb1648fbab5d456bd79d2d017
Soeren Wegener <wegener92@gmail.com>
2018-11-17 11:53:59 +0100

    Hello distance sensor

--- Wheelknife/src/main.cpp
+++ Wheelknife/src/main.cpp
@@ -2,25 +2,38 @@
 
 #define LED_BUILTIN 2
 #define GREEN_LED 19
+#define FRONT_SENSOR_TRIGGER 2
+#define FRONT_SENSOR_ECHO 4
+#define BACK_SENSOR_TRIGGER 32
+#define BACK_SENSOR_ECHO 34
+
+long duration;
+int distance;
 
 void setup() {
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
+  pinMode(FRONT_SENSOR_TRIGGER, OUTPUT);
+  pinMode(FRONT_SENSOR_ECHO, INPUT);
+  pinMode(BACK_SENSOR_TRIGGER, OUTPUT);
+  pinMode(BACK_SENSOR_ECHO, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-  digitalWrite(GREEN_LED, LOW);
-
-  // wait for a second
-  delay(1000);
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(GREEN_LED, HIGH);
-
-   // wait for a second
-  delay(1000);
+  // Clears the trigPin
+  digitalWrite(FRONT_SENSOR_TRIGGER, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(FRONT_SENSOR_TRIGGER, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(FRONT_SENSOR_TRIGGER, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(FRONT_SENSOR_ECHO, HIGH);
+  // Calculating the distance
+  distance = duration*0.034/2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
 }
