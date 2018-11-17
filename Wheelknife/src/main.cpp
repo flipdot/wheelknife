7da01a7b8a193c65c6a3474e95165512fb1b9535
Soeren Wegener <wegener92@gmail.com>
2018-11-17 11:18:59 +0100

    led blink

--- Wheelknife/src/main.cpp
+++ Wheelknife/src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
 
+#define LED_BUILTIN 2
+
 void setup() {
-  // put your setup code here, to run once:
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  // wait for a second
+  delay(1000);
+
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+
+   // wait for a second
+  delay(1000);
 }
