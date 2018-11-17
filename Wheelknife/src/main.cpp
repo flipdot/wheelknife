319935fd233656cd81e9dab1ddc9f26f89006a84
Soeren Wegener <wegener92@gmail.com>
2018-11-17 11:40:55 +0100

    Hello external LED

--- Wheelknife/src/main.cpp
+++ Wheelknife/src/main.cpp
@@ -1,21 +1,25 @@
 #include <Arduino.h>
 
 #define LED_BUILTIN 2
+#define GREEN_LED 19
 
 void setup() {
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
 }
 
 void loop() {
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(GREEN_LED, LOW);
 
   // wait for a second
   delay(1000);
 
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(GREEN_LED, HIGH);
 
    // wait for a second
   delay(1000);
